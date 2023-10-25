c5daa8f758ed72cfbcdf409b80cdcb8700e5a806
Joaquin Sanchez <joaquinsanchezmiravalles@alumnos.tuniversformacion.es>
2023-10-25 09:19:00 +0200

    mostrarSaldo y depositar correctos

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const PIN_CORRECTO = "1234";
 let intentosRestantes = 3;
 
 function mostrarSaldo(){
-
+    console.log(`Su saldo actual es ${saldo.toFixed(2)} €`)
 }
 
 function depositar(){
-
+    const deposito = parseFloat(prompt("Ingrese la cantidad a depositar"))
+    if(isNaN(deposito) || deposito <= 0){
+        console.log("Cantidad inválida. Inténtalo de nuevo")
+    }
+    else{
+        saldo += deposito;
+        console.log(`Se han depositado ${deposito} €`);
+        mostrarSaldo();
+    }
 }
 
 function retirar(){
@@ -23,5 +31,5 @@ function iniciarSesion(){
 }
 
 function operacionesCajero(){
-    
+
 }
