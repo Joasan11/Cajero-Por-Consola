cf23eef5d369b6de70a2c549c1364add12ca2e1f
Joaquin Sanchez <joaquinsanchezmiravalles@alumnos.tuniversformacion.es>
2023-10-25 09:33:27 +0200

    Añadimos el metodo de retirar

--- index.js
+++ index.js
@@ -7,19 +7,27 @@ function mostrarSaldo(){
 }
 
 function depositar(){
-    const deposito = parseFloat(prompt("Ingrese la cantidad a depositar"))
+    const deposito = parseFloat(prompt("Ingrese la cantidad a depositar"));
     if(isNaN(deposito) || deposito <= 0){
-        console.log("Cantidad inválida. Inténtalo de nuevo")
+        console.log("Cantidad inválida. Inténtalo de nuevo");
     }
     else{
         saldo += deposito;
-        console.log(`Se han depositado ${deposito} €`);
+        console.log(`Se han depositado ${deposito.toFixed(2)} €`);
         mostrarSaldo();
     }
 }
 
 function retirar(){
-
+    const retiro = parseFloat(prompt("Ingrese la cantidad a retirar"));
+    if(isNaN(retiro) || retiro <= 0 || retiro > saldo){
+        console.log("Cantidad inválida. Inténtalo de nuevo");
+    }
+    else{
+        saldo -= retiyo;
+        console.log(`Se han retirado ${retiro.toFixed(2)} €`);
+        mostrarSaldo();
+    }
 }
 
 function transferir(){
